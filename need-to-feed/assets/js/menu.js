e732d10d16df2ac71c48358eee826f0c4f962371
jcweathington <jcweathingto@ualr.edu>
2022-02-15 20:17:01 +0000

    N2F

---
+++ need-to-feed/assets/js/menu.js
@@ -0,0 +1,10 @@
+
+/* Set the width of the side navigation */
+function openNav() {
+  document.getElementById("sidenav").style.width = "70%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+}
