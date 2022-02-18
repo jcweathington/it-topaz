e1e49331475b7627041285fd1dcc7da3118993bf
jcweathington <jcweathingto@ualr.edu>
2022-02-18 14:49:29 +0000

    N2F

--- need-to-feed/assets/js/menu.js
+++ need-to-feed/assets/js/menu.js
@@ -1,7 +1,12 @@
 
 /* Set the width of the side navigation */
 function openNav() {
-  document.getElementById("sidenav").style.width = "70%";
+  var x = document.getElementById("sidenav");
+  if (window.matchMedia("(max-width: 400px)").matches) {
+    x.style.width = "90%";
+  } else {
+    x.style.width = "307px";
+  }
 }
 
 /* Set the width of the side navigation to 0 */
