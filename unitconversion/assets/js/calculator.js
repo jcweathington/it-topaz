77791f5115b2d1c7c791775f08ec2cc15963a128
jcweathington <jcweathingto@ualr.edu>
2022-02-25 14:40:23 +0000

    unit conversion I

--- unitconversion/assets/js/calculator.js
+++ unitconversion/assets/js/calculator.js
@@ -7,39 +7,70 @@ function calculate() {
     // If all of the form elements are valid, the get the form values
     if (form.valid()) {
         
-        // Operand 1
-        var operand1 = document.getElementById("Operand1").value;
+        // From Value
+        var FromValue = document.getElementById("FromValue").value;
 
-        // Operator
-        // Get the value associated with the operator that was checked (+, -, *, or /)
-        var operator;
-        if (document.getElementById("AddOperator").checked) {
-            operator = document.getElementById("AddOperator").value;
+        // From Units
+        // Get the value associated with the operator that was checked (cm, m, km, in, ft, yd, mi)
+        var FromUnit;
+        if (document.getElementById("cm").checked) {
+            operator = document.getElementById("cm").value;
         }
-        if (document.getElementById("SubtractOperator").checked) {
-            operator = document.getElementById("SubtractOperator").value;
+        if (document.getElementById("m").checked) {
+            operator = document.getElementById("m").value;
         }
-        if (document.getElementById("MultiplyOperator").checked) {
-            operator = document.getElementById("MultiplyOperator").value;
+        if (document.getElementById("km").checked) {
+            operator = document.getElementById("km").value;
         }
-        if (document.getElementById("DivideOperator").checked) {
-            operator = document.getElementById("DivideOperator").value;
+        if (document.getElementById("in").checked) {
+            operator = document.getElementById("in").value;
+        }
+        if (document.getElementById("ft").checked) {
+            operator = document.getElementById("ft").value;
+        }
+        if (document.getElementById("yd").checked) {
+            operator = document.getElementById("yd").value;
+        }
+        if (document.getElementById("mi").checked) {
+            operator = document.getElementById("mi").value;
         }
         
-        // Operand 2
-        var operand2 = document.getElementById("Operand2").value;
+        // To Units
+        // Get converstion method that was checked (cm, m, km, in, ft, yd, mi)
+        var ToUnit;
+        if (document.getElementById("cm").checked) {
+            operator = document.getElementById("cm").value;
+        }
+        if (document.getElementById("m").checked) {
+            operator = document.getElementById("m").value;
+        }
+        if (document.getElementById("km").checked) {
+            operator = document.getElementById("km").value;
+        }
+        if (document.getElementById("in").checked) {
+            operator = document.getElementById("in").value;
+        }
+        if (document.getElementById("ft").checked) {
+            operator = document.getElementById("ft").value;
+        }
+        if (document.getElementById("yd").checked) {
+            operator = document.getElementById("yd").value;
+        }
+        if (document.getElementById("mi").checked) {
+            operator = document.getElementById("mi").value;
+        }
 
-        CalculateResult(operand1, operator, operand2);
+        CalculateResult(FromValue, FromUnit, ToUnit);
     }
 }
 
-async function CalculateResult(operand1, operator, operand2) {
+async function CalculateResult(FromValue, FromUnit, ToUnit) {
         
         // URL and method used with AJAX Call
-        var myURL = "https://brucebauer.info/assets/ITEC3650/ajaxcalculator.php";
+        var myURL = "https://brucebauer.info/assets/ITEC3650/unitsconversion.php";
 
-        /* AJAX calculator requires Operand1, Operator, and Operand2 */
-        myURL = myURL + "?Operand1=" + encodeURIComponent(operand1) + "&Operator=" + encodeURIComponent(operator) + "&Operand2=" + encodeURIComponent(operand2);
+        /* AJAX calculator requires FromValue FromUnits & ToUnits */
+        myURL = myURL + "?FromValue=" + encodeURIComponent(FromValue) + "&FromUnit=" + encodeURIComponent(FromUnit) + "&ToUnit=" + encodeURIComponent(ToUnit);
 
         /* fetch the results */
         let myCalcObject = await fetch(myURL);
@@ -52,15 +83,18 @@ function clearform() {
     "use strict";
     
     /* Set all of the form values to blank or false */
-    document.getElementById("Operand1").value = "";
+    document.getElementById("FromValue").value = "";
     document.getElementById("Operand1Msg").innerHTML = "";
-    document.getElementById("AddOperator").checked = false;
-    document.getElementById("SubtractOperator").checked = false;
-    document.getElementById("MultiplyOperator").checked = false;
-    document.getElementById("DivideOperator").checked = false;
+    document.getElementById("cm").checked = false;
+    document.getElementById("m").checked = false;
+    document.getElementById("km").checked = false;
+    document.getElementById("in").checked = false;
+    document.getElementById("ft").checked = false;
+    document.getElementById("yd").checked = false;
+    document.getElementById("mi").checked = false;
+    document.getElementById("FromUnit").value = "";
     document.getElementById("OperatorMsg").innerHTML = "";
-    document.getElementById("Operand2").value = "";
-    document.getElementById("Operand2Msg").innerHTML = "";
+    document.getElementById("ToUnit").value = "";
     document.getElementById("Result").innerHTML = "";
 }
 
