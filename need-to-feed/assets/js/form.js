028a61cb0f277fce1e7e830db44bef5e9e7309d9
jcweathington <jcweathingto@ualr.edu>
2022-03-06 04:12:28 +0000

    n2f

---
+++ need-to-feed/assets/js/form.js
@@ -0,0 +1,11 @@
+
+
+ var r10o9x0d18aihh1; (function(d, t) { var s = d.createElement(t), 
+ options = { 'userName':'jcwualr', 'formHash':'r10o9x0d18aihh1', 
+ 'autoResize':true, 'height':'521', 'async':true, 'host':'wufoo.com', 
+ 'header':'show', 'ssl':true }; s.src = ('https:' == d.location.protocol ?'https://':'http://') + 
+ 'secure.wufoo.com/scripts/embed/form.js'; s.onload = s.onreadystatechange = function() 
+ { var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return; 
+ try { r10o9x0d18aihh1 = new WufooForm(); r10o9x0d18aihh1.initialize(options); r10o9x0d18aihh1.display(); } 
+ catch (e) { } }; var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr); })
+ (document, 'script'); 
