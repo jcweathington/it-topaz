da1a88880b76eb534522885c86e910f7bce59652
jcweathington <jcweathingto@ualr.edu>
2022-03-16 23:13:16 +0000

    currency

--- currency-value-history/assets/js/conversion.js
+++ currency-value-history/assets/js/conversion.js
@@ -37,7 +37,7 @@ async function GetStock() {
 
         // Convert Currency
         // Get the value associated with the operator that was checked (USD, MXN, CAD, EUR, CNY)
-        var baseUnit;
+        var convertCurrency;
         if (document.getElementById("USD2").checked) {
             convertCurrency = document.getElementById("USD2").value;
         }
@@ -143,7 +143,4 @@ function ClearForm() {
     var canvas0 = document.getElementById("chartjs-0");
     var context0 = canvas0.getContext('2d');    
     context0.clearRect(0, 0, canvas0.width, canvas0.height);
-    var canvas1 = document.getElementById("chartjs-1");
-    var context1 = canvas1.getContext('2d');    
-    context1.clearRect(0, 0, canvas1.width, canvas1.height);
 }
