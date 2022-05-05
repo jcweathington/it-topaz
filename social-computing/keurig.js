cbbef066c5a07ca6cb0145a2d5b6c8016a90594c
jcweathington <jcweathingto@ualr.edu>
2022-05-05 11:12:04 +0000

    tweet

---
+++ social-computing/keurig.js
@@ -0,0 +1,1 @@
+curl --request GET 'https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev' --header 'Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAKHrcAEAAAAA%2Bhgy%2FKUoBEVoUxvVtiNifGOcVSc%3DyOgWzqGOALHZ0bceaI4sysyX8GZuyGvST4kegu3QTZY9PlPh73'
