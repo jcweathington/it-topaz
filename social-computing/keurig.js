905353dbebc2c6659ae939c4bb7e6980f9d542c9
jcweathington <jcweathingto@ualr.edu>
2022-05-05 11:14:29 +0000

    tweet

--- social-computing/keurig.js
+++ social-computing/keurig.js
@@ -1,1 +1,5 @@
-curl --request GET 'https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev' --header 'Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAKHrcAEAAAAA%2Bhgy%2FKUoBEVoUxvVtiNifGOcVSc%3DyOgWzqGOALHZ0bceaI4sysyX8GZuyGvST4kegu3QTZY9PlPh73'
+
+async function displayLevels() {
+    curl --request GET 'https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev' --header 'Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAKHrcAEAAAAA%2Bhgy%2FKUoBEVoUxvVtiNifGOcVSc%3DyOgWzqGOALHZ0bceaI4sysyX8GZuyGvST4kegu3QTZY9PlPh73'
+     
+  }
